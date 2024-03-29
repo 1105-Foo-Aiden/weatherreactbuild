a96f90bc8e0f7e3ade5226637a7b4238e9ccfe7e
Aiden Foo <aidenfoo154@gmail.com>
2024-03-29 15:32:36 -0700

    For Caleb

--- src/app/Services/DataServices.ts
+++ src/app/Services/DataServices.ts
@@ -1,6 +1,5 @@
 import { ICityGet, IForecast } from "../Interfaces/Interfaces";
-let Apikey = process.env.NEXT_PUBLIC_API_KEY
-
+let Apikey = process.env.NEXT_PUBLIC_API_KEY;
 
 export const getData = async (longitude: number, latitude: number) => {
   const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${Apikey}&units=imperial&cnt=40`);
@@ -9,7 +8,7 @@ export const getData = async (longitude: number, latitude: number) => {
 };
 
 export const GetMaxMin = (data: IForecast) => {
-  const compareDay:  Date = new Date();
+  const compareDay: Date = new Date();
   const compareDay2: Date = new Date(compareDay.getTime() + 86400000);
   const compareDay3: Date = new Date(compareDay.getTime() + 86400000 * 2);
   const compareDay4: Date = new Date(compareDay.getTime() + 86400000 * 3);
@@ -38,36 +37,35 @@ export const GetMaxMin = (data: IForecast) => {
 
   for (let i = 0; i < data.list.length; i++) {
     let dayTime2 = new Date(data.list[i].dt * 1000);
-    if (dayTime2.toLocaleDateString("en-US") === compareDay.toLocaleDateString("en-US")) {
+    if (dayTime2.toLocaleDateString("en-US") ===compareDay.toLocaleDateString("en-US")) {
       day1MaxArr.push(data.list[i].main.temp_max);
       day1MinArr.push(data.list[i].main.temp_min);
-    } else if ( dayTime2.toLocaleDateString("en-US") === compareDay2.toLocaleDateString("en-US")) {
+    } else if (dayTime2.toLocaleDateString("en-US") ===compareDay2.toLocaleDateString("en-US")) {
       day2MaxArr.push(data.list[i].main.temp_max);
       day2MinArr.push(data.list[i].main.temp_min);
       day1StatusArr.push(data.list[i].weather[0].icon);
-    } else if ( dayTime2.toLocaleDateString("en-US") === compareDay3.toLocaleDateString("en-US") ) {
+    } else if (dayTime2.toLocaleDateString("en-US") ===compareDay3.toLocaleDateString("en-US")) {
       day3MaxArr.push(data.list[i].main.temp_max);
       day3MinArr.push(data.list[i].main.temp_min);
       day2StatusArr.push(data.list[i].weather[0].icon);
-    } else if ( dayTime2.toLocaleDateString("en-US") === compareDay4.toLocaleDateString("en-US") ) {
+    } else if (dayTime2.toLocaleDateString("en-US") ===compareDay4.toLocaleDateString("en-US")) {
       day4MaxArr.push(data.list[i].main.temp_max);
       day4MinArr.push(data.list[i].main.temp_min);
       day3StatusArr.push(data.list[i].weather[0].icon);
-    } else if ( dayTime2.toLocaleDateString("en-US") === compareDay5.toLocaleDateString("en-US") ) {
+    } else if (dayTime2.toLocaleDateString("en-US") ===compareDay5.toLocaleDateString("en-US")) {
       day5MaxArr.push(data.list[i].main.temp_max);
       day5MinArr.push(data.list[i].main.temp_min);
       day4StatusArr.push(data.list[i].weather[0].icon);
-    } else if ( dayTime2.toLocaleDateString("en-US") === compareDay6.toLocaleDateString("en-US") ) {
+    } else if (dayTime2.toLocaleDateString("en-US") === compareDay6.toLocaleDateString("en-US")) {
       day6MaxArr.push(data.list[i].main.temp_max);
       day6MinArr.push(data.list[i].main.temp_min);
       day5StatusArr.push(data.list[i].weather[0].icon);
     }
   }
 
-
   function StatusMode(statusArr: string[]) {
-    const frequency:any = {};
-    
+    const frequency: any = {};
+
     statusArr.forEach((status) => {
       frequency[status] = (frequency[status] || 0) + 1;
     });
@@ -90,27 +88,45 @@ export const GetMaxMin = (data: IForecast) => {
   const StatusDay4 = StatusMode(day4StatusArr);
   const StatusDay5 = StatusMode(day5StatusArr);
 
-  const currentMax = Math.floor(Math.max(...day1MaxArr))
-  const currentMin = Math.floor(Math.min(...day1MinArr))
-  const DayOneMax = Math.floor(Math.max(...day2MaxArr))
-  const DayOneMin = Math.floor(Math.min(...day2MaxArr))
-  const DayTwoMax = Math.floor(Math.max(...day3MaxArr))
-  const DayTwoMin = Math.floor(Math.min(...day3MinArr))
-  const DayThreeMax = Math.floor(Math.max(...day4MaxArr))
-  const DayThreeMin = Math.floor(Math.min(...day4MinArr))
-  const DayFourMax = Math.floor(Math.max(...day5MaxArr))
-  const DayFourMin = Math.floor(Math.min(...day5MaxArr))
-  const DayFiveMax = Math.floor(Math.max(...day6MinArr))
-  const DayFiveMin = Math.floor(Math.min(...day6MinArr))
+  const currentMax = Math.floor(Math.max(...day1MaxArr));
+  const currentMin = Math.floor(Math.min(...day1MinArr));
+  const DayOneMax = Math.floor(Math.max(...day2MaxArr));
+  const DayOneMin = Math.floor(Math.min(...day2MaxArr));
+  const DayTwoMax = Math.floor(Math.max(...day3MaxArr));
+  const DayTwoMin = Math.floor(Math.min(...day3MinArr));
+  const DayThreeMax = Math.floor(Math.max(...day4MaxArr));
+  const DayThreeMin = Math.floor(Math.min(...day4MinArr));
+  const DayFourMax = Math.floor(Math.max(...day5MaxArr));
+  const DayFourMin = Math.floor(Math.min(...day5MaxArr));
+  const DayFiveMax = Math.floor(Math.max(...day6MinArr));
+  const DayFiveMin = Math.floor(Math.min(...day6MinArr));
 
-  return {compareDay2, compareDay3, compareDay4, compareDay5, compareDay6, StatusDay1, StatusDay2, StatusDay3, StatusDay4, StatusDay5, currentMax, currentMin, DayOneMax, DayOneMin, DayTwoMax, DayTwoMin, DayThreeMax, DayThreeMin,  DayFourMax, DayFourMin, DayFiveMax, DayFiveMin, }
+  return {compareDay2, compareDay3, compareDay4, compareDay5, compareDay6, StatusDay1, StatusDay2, StatusDay3, StatusDay4, StatusDay5, currentMax, currentMin, DayOneMax, DayOneMin, DayTwoMax, DayTwoMin, DayThreeMax, DayThreeMin, DayFourMax, DayFourMin, DayFiveMax, DayFiveMin, };
 };
 
-
-export const GetCity = async (city:string) =>{
+export const GetCity = async (city: string) => {
   const CityLookup = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${Apikey}`);
-  const cityName: ICityGet[] = await CityLookup.json()
+  const cityName: ICityGet[] = await CityLookup.json();
   let latitude = cityName[0].lat;
   let longitude = cityName[0].lon;
-  return {longitude, latitude}
-}
+  return { longitude, latitude };
+};
+
+const saveToLocalStorage = (city: string) => {
+  let favorites = getlocalStorage();
+  if (!favorites.includes(city)) {favorites.push(city);}
+  localStorage.setItem("Favorites", JSON.stringify(favorites));
+};
+const getlocalStorage = () => {
+  let localStorageData = localStorage.getItem("Favorites");
+  if (localStorageData == null) {return [];}
+  return JSON.parse(localStorageData);
+};
+
+const removeFromLocalStorage = (city: string) => {
+  let favorites = getlocalStorage();
+  let namedIndex = favorites.indexOf(city);
+  favorites.splice(namedIndex, 1);
+  localStorage.setItem("Favorites", JSON.stringify(favorites));
+};
+export { saveToLocalStorage, getlocalStorage, removeFromLocalStorage };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-
 import Satr from "./Star.png";
 import { useEffect, useState } from "react";
 import { IForecast } from "./Interfaces/Interfaces";
 import Image from "next/image";
 import DayForcastComponent from "./Components/DayForcastComponent";
-import { GetCity, GetMaxMin, getData } from "./Services/DataServices";
-import favStar from './FilledStar.png'
+import { GetCity, GetMaxMin, getData, getlocalStorage, removeFromLocalStorage, saveToLocalStorage } from "./Services/DataServices";
+import favStar from "./FilledStar.png";
 
 
 export default function Home() {
@@ -39,18 +38,20 @@ export default function Home() {
   const [compareDay5, setCompareDay5] = useState<Date>();
   const [compareDay6, setCompareDay6] = useState<Date>();
   let [searchvalue, setSearchValue] = useState<string>();
-  const [fav, setFav] = useState<Boolean>(false)
+  const [fav, setFav] = useState<Boolean>();
   const [date, setdate] = useState<any>();
-
+  const [favorites, setFavorites] = useState<string[]>([])
+  const [searchCityValue, setSearchCityValue] = useState<string>()
 
   useEffect(() => {
     global.navigator.geolocation.getCurrentPosition(success, error);
+    setFavorites(getlocalStorage())
   }, []);
 
   function success(position: any) {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
-    setdate(new Date().toLocaleDateString("default", {day: "numeric", month: "numeric",}));
+    setdate(new Date().toLocaleDateString("default", { day: "numeric", month: "numeric", }));
     setSuccessBool(true);
   }
 
@@ -60,55 +61,75 @@ export default function Home() {
   useEffect(() => {
     if (successBool) {
       const fetchData = async () => {
-        console.log(longitude, latitude);
         const weatherData = await getData(longitude, latitude);
         setForecastData(weatherData);
-        setCurrentStatus(
-          `https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`
-        );
-        const { compareDay2, compareDay3, compareDay4, compareDay5, compareDay6, StatusDay1, StatusDay2, StatusDay3, StatusDay4, StatusDay5, currentMax, currentMin, DayOneMax, DayOneMin, DayTwoMax, DayTwoMin, DayThreeMax, DayThreeMin, DayFourMax, DayFourMin, DayFiveMax, DayFiveMin, } = GetMaxMin(weatherData);
+        setCurrentStatus(`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`);
+        const {compareDay2, compareDay3, compareDay4, compareDay5, compareDay6, StatusDay1, StatusDay2, StatusDay3, StatusDay4, StatusDay5, currentMax, currentMin, DayOneMax, DayOneMin, DayTwoMax, DayTwoMin, DayThreeMax, DayThreeMin, DayFourMax, DayFourMin, DayFiveMax, DayFiveMin, } = GetMaxMin(weatherData);
         setStatusDay1(`https://openweathermap.org/img/wn/${StatusDay1}@2x.png`),
-        setStatusDay2(`https://openweathermap.org/img/wn/${StatusDay2}@2x.png`),
-        setStatusDay3(`https://openweathermap.org/img/wn/${StatusDay3}@2x.png`),
-        setStatusDay4(`https://openweathermap.org/img/wn/${StatusDay4}@2x.png`),
-        setStatusDay5(`https://openweathermap.org/img/wn/${StatusDay5}@2x.png`);
-        setCurrentDayMax(currentMax), setCurrentDayMin(currentMin), setDayOneMax(DayOneMax), setDayOneMin(DayOneMin), setDayTwoMax(DayTwoMax), setDayTwoMin(DayTwoMin), setDayThreeMax(DayThreeMax), setDayThreeMin(DayThreeMin), setDayFourMax(DayFourMax), setDayFourMin(DayFourMin), setDayFiveMax(DayFiveMax), setDayFiveMin(DayFiveMin); setCompareDay2(compareDay2), setCompareDay3(compareDay3), setCompareDay4(compareDay4), setCompareDay5(compareDay5), setCompareDay6(compareDay6); };
+        setStatusDay2(`https://openweathermap.org/img/wn/${StatusDay2}@2x.png`), setStatusDay3(`https://openweathermap.org/img/wn/${StatusDay3}@2x.png`), setStatusDay4(`https://openweathermap.org/img/wn/${StatusDay4}@2x.png`), setStatusDay5(`https://openweathermap.org/img/wn/${StatusDay5}@2x.png`), setCurrentDayMax(currentMax), setCurrentDayMin(currentMin), setDayOneMax(DayOneMax), setDayOneMin(DayOneMin), setDayTwoMax(DayTwoMax), setDayTwoMin(DayTwoMin), setDayThreeMax(DayThreeMax),
+        setDayThreeMin(DayThreeMin), setDayFourMax(DayFourMax), setDayFourMin(DayFourMin), setDayFiveMax(DayFiveMax), setDayFiveMin(DayFiveMin), setCompareDay2(compareDay2), setCompareDay3(compareDay3), setCompareDay4(compareDay4), setCompareDay5(compareDay5), setCompareDay6(compareDay6) 
+      };
       fetchData();
     }
   }, [successBool]);
-  const handleSearch = async () =>{
-    setLatitude(0)
-    setLongitude(0)
-    const {longitude, latitude} = await GetCity(searchvalue? searchvalue : "")
-    setLatitude(latitude)
-    setLongitude(longitude)
-  }
+  const handleSearch = async () => {
+    setLatitude(0);
+    setLongitude(0);
+    const { longitude, latitude } = await GetCity(searchvalue ? searchvalue : "");
+    setLatitude(latitude);
+    setLongitude(longitude);
+  };
 
+  const handleFav = () => {
+    setFav(!fav);
+    setFavorites(getlocalStorage())
+    if( forecastData && !favorites.includes(forecastData!.city.name)){
+      saveToLocalStorage(forecastData.city.name)
+    }
+    else{
+      removeFromLocalStorage(forecastData!.city.name)
+    } 
+    setFavorites(getlocalStorage())
+  };
 
-  const handleFav = () =>{
-    setFav(!fav)
-  }
+  useEffect(() =>{
+    const GetNewData = async() =>{
+      
+    }
+  }, [searchCityValue])
 
   return (
     <>
-       <div className="grid grid-cols-3 pt-5 mb-10">
-      <div>
-
-      </div>
-      <div className="flex gap-8 justify-around ">
-        <h1 className="text-4xl">The Weather App</h1>
+      <div className="grid grid-cols-3 pt-5 mb-10">
+        <div></div>
+        <div className="flex gap-8 justify-around ">
+          <h1 className="text-4xl">The Weather App</h1>
         </div>
         <div className="gap-4 pr-5 flex justify-end">
           <input type="text" placeholder="Search a City" className="h-10 w-56 text-black" onChange={(e) => setSearchValue(e.target.value)} value={searchvalue} />
-          <button className="w-28 h-10 bg-black" onClick={handleSearch}>Go</button>
+          <button className="w-28 h-10 bg-black" onClick={handleSearch}>
+            Go
+          </button>
         </div>
-      
-    </div>
-      <div className="grid grid-cols-3 grid-rows-1">
-        <div className="flex justify-center">
-          <h1 className="text-4xl underline flex justify-center w-fit">
+      </div>
+      <div className="grid grid-cols-3 grid-flow-rows">
+        <div>
+          <div className="flex justify-center">
+           <h1 className="text-4xl underline w-fit shadow-lg">
             Favorites
-          </h1>
+          </h1> 
+          </div>
+          <div className="grid grid-cols-2 grid-flow-row pt-5 pr-10">
+            {
+              favorites.map(city =>{
+                return(
+                  <div className="bg-blue-950 w-fit h-fit rounded-xl shadow-xl ml-10">
+                    <p className="text-3xl flex text-middle py-1 px-2">{city}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
         <div className="grid grid-rows-4 w-full justify-center h-40 gap-4">
           <div className="flex justify-center h-4">
@@ -116,11 +137,11 @@ export default function Home() {
               {forecastData && forecastData.city.name}
             </h1>
             <p>
-              <Image width={50} height={50} src={!fav? Satr.src : favStar.src } alt="favStar" onClick={handleFav} />
+              <Image width={50} height={50} src={forecastData && favorites.includes(forecastData.city.name) ? favStar.src : Satr.src} alt="favStar" onClick={handleFav} />
             </p>
           </div>
           <div className="flex justify-center">
-            <img src={currentStatus} alt="Current Status" className=" h-96" />
+            <img src={currentStatus} alt="Current Status" className="w-auto h-96 " />
           </div>
         </div>
         <div className="pt-20 pr-20 grid justify-center grid-rows-5 gap-0">
@@ -134,20 +155,28 @@ export default function Home() {
             <p className="text-[60px] flex justify-center">
               {forecastData && Math.round(forecastData.list[0].main.temp)}F/
             </p>
-            <p className="text-[40px] opacity-50 pt-6">{forecastData && Math.round(((forecastData.list[0].main.temp - 32) * 5) / 9)}C</p>
+            <p className="text-[40px] opacity-50 pt-6">
+              {forecastData &&
+                Math.round(((forecastData.list[0].main.temp - 32) * 5) / 9)}
+              C
+            </p>
           </div>
           <div className="flex justify-center">
             <p className="text-[30px]">Hi: {currentDayMax}F/</p>
-            <p className="text-[25px] opacity-50 pt-2">{currentDayMax && Math.floor(((currentDayMax - 32) * 5) / 9)}C</p>
+            <p className="text-[25px] opacity-50 pt-2">
+              {currentDayMax && Math.floor(((currentDayMax - 32) * 5) / 9)}C
+            </p>
           </div>
           <div className="flex justify-center">
             <p className="text-[30px]">Lo: {currentDayMin}F/</p>
-            <p className="text-[25px] opacity-50 pt-2">{currentDayMin && Math.round(((currentDayMin - 32) * 5) / 9)}C</p>
+            <p className="text-[25px] opacity-50 pt-2">
+              {currentDayMin && Math.round(((currentDayMin - 32) * 5) / 9)}C
+            </p>
           </div>
         </div>
       </div>
       <div className="grid grid-cols-5 justify-center mt-10">
-        <DayForcastComponent date={compareDay2} Max={DayOneMax} Min={DayOneMin} Status={StatusDay1} />
+        <DayForcastComponent date={compareDay2} Max={DayOneMax} Min={DayOneMin} Status={StatusDay1}/>
         <DayForcastComponent date={compareDay3} Max={DayTwoMax} Min={DayTwoMin} Status={StatusDay2} />
         <DayForcastComponent date={compareDay4} Max={DayThreeMax} Min={DayThreeMin} Status={StatusDay3} />
         <DayForcastComponent date={compareDay5} Max={DayFourMax} Min={DayFourMin} Status={StatusDay4} />
